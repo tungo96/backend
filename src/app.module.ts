55e5676ae22b3332fa9f2dd900ffdedea7ce589a
tungo96 <tu.ngovan@hdwebsoft.dev>
2025-06-16 12:09:33 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
